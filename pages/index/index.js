e1bec20c8e150f58993e90acc5cde3ffede6afa0
li mkig <notreal@faker.com>
2021-04-26 17:01:42 +0800

    v5

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,35 +11,59 @@ Page({
         index_game_list: [],
         start: 0,
         limit: 15,
+        categorys:[
+            {'color':'#9BD4B2',type:"Role-Playing",name:"角色扮演"},
+            {'color':'#9BD4B2',type:"Sports",name:"体育"},  
+            {'color':'#2C4553',type:"Adventure",name:"冒险"},
+            {'color':'#B5C228',type:"Indie",name:"独立"},
+            {'color':'#738AF2',type:"Fighting",name:"格斗"},
+            {'color':'#42406E',type:"Shooter",name:"射击"},
+            {'color':'#AC2BE0',type:"Action",name:"动作"},
+            {'color':'#9BD4B2',type:"First-Person",name:"第一人称"},
+            {'color':'#9BD4B2',type:"Party",name:"聚会"},
+            {'color':'#9BD4B2',type:"Multiplayer",name:"多人游戏"},
+            {'color':'#9BD4B2',type:"Racing",name:"赛车"}
+            ],
+        click: '',
+        test:1
     },
+    tapCategory(e){
+        console.log(e)
+        let category = e.currentTarget.dataset.id
+        let token = wx.getStorageSync('token')
+        let start = this.data.start
+        let limit = this.data.limit
+        let _this = this
 
-    tap_game(e){
-        let id = e.currentTarget.dataset.id
-        wx.navigateTo({
-          url: '/pages/switch/switch?id='+ id,
+        this.setData({
+            start:0,
+            click:category
         })
-        // let token = wx.getStorageSync('token')
-        // let _this = this
-        // let url = getApp().globalData.server + "/game-opt/hit/" + id;
-        // wx.request({
-        //     url: url,
-        //     method: 'POST',
-        //     header: {
-        //         auth: token
-        //     },
-        //     success(res) {
-        //         if (601 == res.statusCode) {
-        //             getApp().userlogin()
-        //         }
+        
+        let url = getApp().globalData.server + "/index/sales/" + category + "/" + start + "/" + limit;
+        wx.request({
+            url: url,
+            method: 'GET',
+            header: {
+                auth: token
+            },
+            success(res) {
+                if (601 == res.statusCode) {
+                    getApp().userlogin()
+                }
 
-        //         if (200 == res.statusCode) {
-        //             _this.setData({
-        //                 index_game_list: res.data
-        //             })
-        //         }
-        //     }
-        // })
+                if (200 == res.statusCode) {
+                    _this.setData({
+                        index_game_list: res.data
+                    })
+                }
+            }
+        })
+
+
     },
+
+   
 
     /**
      * 生命周期函数--监听页面加载
@@ -62,11 +86,17 @@ Page({
     onShow: function () {
     
         let token = wx.getStorageSync('token')
-        let start = this.data.start
+        let start = 0
         let limit = this.data.limit
         let _this = this
+
+        this.setData({
+            start:0,
+            click:'Role-Playing'
+        })
+
         
-        let url = getApp().globalData.server + "/index/sales/" + start + "/" + limit;
+        let url = getApp().globalData.server + "/index/sales/Role-Playing/" + start + "/" + limit;
         wx.request({
             url: url,
             method: 'GET',
@@ -116,7 +146,7 @@ Page({
     onReachBottom: function () {
 
         let _old = this.data.index_game_list
-        let type = this.data.check_tap
+        let category = this.data.click
         let token = wx.getStorageSync('token')
         let limit = this.data.limit
         let start = this.data.start + limit
@@ -125,7 +155,7 @@ Page({
         })
         let _this = this
         // /index/{type}/{start}/{limit}
-        let url = getApp().globalData.server + "/index/sales/" + start + "/" + limit;
+        let url = getApp().globalData.server + "/index/sales/" + category + "/" + start + "/" + limit;
         wx.request({
             url: url,
             method: 'GET',
--- pages/switch/switch.js
+++ pages/switch/switch.js
@@ -58,6 +58,103 @@ Page({
         })
 
     },
+    love(e){
+        let game = this.data.game 
+        game.userLike = !game.userLike 
+        this.setData({
+            game
+        })
+
+        let id = this.data.id
+        let token = wx.getStorageSync('token')
+        let _this = this
+        let url = getApp().globalData.server + "/game-opt/like/" + id;
+        wx.request({
+            url: url,
+            method: 'POST',
+            header: {
+                auth: token
+            },
+            success(res) {
+                if (601 == res.statusCode) {
+                    getApp().userlogin()
+                }
+
+                if (200 == res.statusCode) {
+
+                    let game = res.data
+                    game.gallery = game.gallery.replace(/,http/g,';http').split(';')
+
+                    _this.setData({
+                        game:game
+                    })
+                }
+            }
+        })
+
+    },
+    dislove(e){
+        let game = this.data.game 
+        game.userLike = !game.userLike 
+        this.setData({
+            game
+        })
+
+        let id = this.data.id
+        let token = wx.getStorageSync('token')
+        let _this = this
+        let url = getApp().globalData.server + "/game-opt/dislike/" + id;
+        wx.request({
+            url: url,
+            method: 'POST',
+            header: {
+                auth: token
+            },
+            success(res) {
+                if (601 == res.statusCode) {
+                    getApp().userlogin()
+                }
+
+                if (200 == res.statusCode) {
+
+                    let game = res.data
+                    game.gallery = game.gallery.replace(/,http/g,';http').split(';')
+
+                    _this.setData({
+                        game:game
+                    })
+                }
+            }
+        })
+    },
+    mark(e){
+        let game = this.data.game 
+        game.userMark = !game.userMark 
+        this.setData({
+            game
+        })
+    },
+    dismark(e){
+        let game = this.data.game 
+        game.userMark = !game.userMark 
+        this.setData({
+            game
+        })
+    },
+    have(e){
+        let game = this.data.game 
+        game.userHave = !game.userHave
+        this.setData({
+            game
+        })
+    },
+    dishave(e){
+        let game = this.data.game 
+        game.userHave = !game.userHave
+        this.setData({
+            game
+        })
+    },
     /**
      * 生命周期函数--监听页面隐藏
      */
